bf7bbc1d34bdd84ff58be288bb80254e767a5582
Vimal cedric LA <vimalcedric26@gmail.com>
2026-04-29 17:47:58 +0530

    Fixed myinfo page synchronization

--- orange_hrm/pages/my_info.py
+++ orange_hrm/pages/my_info.py
@@ -5,13 +5,31 @@ class MyinfoPage:
         self.page = page
 
     def verify_info_click(self):
-        self.page.get_by_role("link", name="My Info").click()
+        myinfo_menu = self.page.get_by_role("link", name="My Info")
+        expect(myinfo_menu).to_be_visible(timeout=15000)
+        myinfo_menu.click()
+
+        self.page.wait_for_load_state("networkidle")
+        self.page.wait_for_timeout(3000)
 
     def verify_each_info_click(self):
-        info_list = ["Personal Details", "Contact Details", "Emergency Contacts", "Dependents", "Immigration", "Job", "Salary", "Report-to", "Qualifications", "Memberships"]
+        info_list = [
+            "Personal Details",
+            "Contact Details",
+            "Emergency Contacts",
+            "Dependents",
+            "Immigration",
+            "Job",
+            "Salary",
+            "Report-to",
+            "Qualifications",
+            "Memberships"
+        ]
 
         for info in info_list:
             item = self.page.get_by_role("link", name=info)
-            expect(item).to_be_visible()
-            expect(item).to_be_enabled()
+            expect(item).to_be_visible(timeout=15000)
+            expect(item).to_be_enabled(timeout=15000)
             item.click()
+            self.page.wait_for_load_state("networkidle")
+            self.page.wait_for_timeout(1500)
